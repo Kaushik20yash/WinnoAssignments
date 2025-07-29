cf36114a1e0a9dbc3621bacd45effdd13ccfa15f
Kaushik20yash <147990478+Kaushik20yash@users.noreply.github.com>
2025-07-29 10:59:55 +0530

    Add files via upload

--- MainWeChat/ToDo/index.js
+++ MainWeChat/ToDo/index.js
@@ -1,5 +1,16 @@
 let ul = document.getElementById('ul');
 let btn = document.getElementById('btn');
+let currentli = null;
+let currentp = null;
+let save = document.getElementsByClassName('edit-details')[0];
+    save.addEventListener('click', function() {
+        if(currentli && currentp) {
+        currentp.textContent = document.getElementById('task-input').value;
+        }
+        document.getElementById('task-input').value = '';
+        document.getElementById('edit-task').style.display = 'none';
+        ul.style.display = 'flex';
+    });
 btn.addEventListener('click', function() {
     let input = document.getElementById('input');
     let li = document.createElement('li');
@@ -7,10 +18,8 @@ btn.addEventListener('click', function() {
     let p = document.createElement('p');
     let edit = document.createElement('button');
     edit.textContent = 'Edit';
-    edit.id = "editbtn";
     let deletebtn = document.createElement('button');
     deletebtn.textContent = 'Delete';
-    deletebtn.id = "deletebtn";
     div.style.display = 'flex';
     div.style.width = '100%';
     div.style.gap = '0.5em';
@@ -19,6 +28,8 @@ btn.addEventListener('click', function() {
     edit.style.width = '15%';
     deletebtn.style.width = '15%';
     p.textContent = input.value;
+    edit.classList.add('editbtn');
+    deletebtn.classList.add('deletebtn');
     div.appendChild(p);
     div.appendChild(edit);
     div.appendChild(deletebtn);
@@ -26,15 +37,13 @@ btn.addEventListener('click', function() {
     ul.appendChild(li);
 
     edit.addEventListener('click', function() {
+        currentli = li;
+        currentp = p;
         ul.style.display = 'none';
         document.getElementById('edit-task').style.display = 'flex';
     });
-    let save = document.getElementsByClassName('edit-details')[0];
-    save.addEventListener('click', function() {
-        li.textContent = document.getElementById('task-input').value;
-        document.getElementById('task-input').value = '';
-        document.getElementById('edit-task').style.display = 'none';
-        ul.style.display = 'flex';
+    deletebtn.addEventListener('click', function() {
+        li.remove();
     });
     input.value = "";
 });
